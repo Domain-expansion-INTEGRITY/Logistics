5713c8cf954ab6c480a0f275b3e1801508e79c56
shurona <khc4669@naver.com>
2024-09-09 11:26:36 +0900

    fix: 불필요한 주석 제거

--- com.domain_expansion.integrity.user/src/main/java/com/domain_expansion/integrity/user/infrastructure/repository/JpaUserRepository.java
+++ com.domain_expansion.integrity.user/src/main/java/com/domain_expansion/integrity/user/infrastructure/repository/JpaUserRepository.java
@@ -1,7 +1,6 @@
 package com.domain_expansion.integrity.user.infrastructure.repository;
 
 import com.domain_expansion.integrity.user.domain.model.User;
-import com.domain_expansion.integrity.user.domain.repository.UserCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<User, Long> {
