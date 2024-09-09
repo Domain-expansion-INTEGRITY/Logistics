8ae618002136fbbb56d1da85c42e179c15a86e58
bongdaehyun <pkjeogus@gmail.com>
2024-09-09 14:35:18 +0900

    design : dto -> entity 용 mapper 클래스 생성

---
+++ com.domain_expansion.integrity.company/src/main/java/com/domain_expansion/integrity/company/application/service/mapper/CompanyMapper.java
@@ -0,0 +1,20 @@
+package com.domain_expansion.integrity.company.application.service.mapper;
+
+import com.domain_expansion.integrity.company.domain.model.Company;
+import com.domain_expansion.integrity.company.domain.model.vo.CompanyAddress;
+import com.domain_expansion.integrity.company.prsentation.request.CompanyCreateRequestDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyMapper {
+
+    public Company CompanyCreateDtoToCompany(CompanyCreateRequestDto requestDto){
+        return new Company(
+                requestDto.userId(),
+                requestDto.hubId(),
+                requestDto.name(),
+                requestDto.companyType(),
+                new CompanyAddress(requestDto.address())
+                );
+    }
+}
