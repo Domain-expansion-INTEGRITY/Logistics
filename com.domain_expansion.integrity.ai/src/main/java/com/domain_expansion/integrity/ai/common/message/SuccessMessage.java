43be467a4a753938c03c42ed138bc900f4ebff97
shurona <khc4669@naver.com>
2024-09-10 10:21:03 +0900

    design: ai common type 추가

---
+++ com.domain_expansion.integrity.ai/src/main/java/com/domain_expansion/integrity/ai/common/message/SuccessMessage.java
@@ -0,0 +1,16 @@
+package com.domain_expansion.integrity.ai.common.message;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum SuccessMessage {
+
+    SUCCESS_CREATE_AI(HttpStatus.CREATED, "AI 생성에 성공하였습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
