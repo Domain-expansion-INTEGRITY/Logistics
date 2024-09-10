7ee63d5b150c1a688d20593cdeed68cf2d8b8498
shurona <khc4669@naver.com>
2024-09-10 21:58:53 +0900

    fix: Builder 위치 변경

--- com.domain_expansion.integrity.auth/src/main/java/com/domain_expansion/integrity/auth/application/client/request/UserLoginRequestDto.java
+++ com.domain_expansion.integrity.auth/src/main/java/com/domain_expansion/integrity/auth/application/client/request/UserLoginRequestDto.java
@@ -3,12 +3,12 @@ package com.domain_expansion.integrity.auth.application.client.request;
 import lombok.AccessLevel;
 import lombok.Builder;
 
+@Builder(access = AccessLevel.PROTECTED)
 public record UserLoginRequestDto(
     String username,
     String password
 ) {
 
-    @Builder(access = AccessLevel.PROTECTED)
     public static UserLoginRequestDto from(String username, String password) {
         return UserLoginRequestDto.builder()
             .username(username)
