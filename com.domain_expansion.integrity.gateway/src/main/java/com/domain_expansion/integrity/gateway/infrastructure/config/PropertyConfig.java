ce4901f34542063d2cac6e094cd03df94e12a4b4
shurona <khc4669@naver.com>
2024-09-11 16:13:45 +0900

    feat: gateway infrastructure

---
+++ com.domain_expansion.integrity.gateway/src/main/java/com/domain_expansion/integrity/gateway/infrastructure/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.domain_expansion.integrity.gateway.infrastructure.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.domain_expansion.integrity.gateway")
+public class PropertyConfig {
+
+}
