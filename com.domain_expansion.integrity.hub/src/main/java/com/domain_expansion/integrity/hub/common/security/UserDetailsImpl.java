0992d6ba77a3f38235c1eeb749f2cb1a1aefa194
bongdaehyun <pkjeogus@gmail.com>
2024-09-12 15:11:34 +0900

    feat : security 와 jwt설정

---
+++ com.domain_expansion.integrity.hub/src/main/java/com/domain_expansion/integrity/hub/common/security/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package com.domain_expansion.integrity.hub.common.security;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private Long userId;
+    private Collection<? extends GrantedAuthority> authorities;
+    private String role;
+
+
+    public UserDetailsImpl(Long userId, Collection<? extends GrantedAuthority> authorities, String repRole) {
+        this.userId = userId;
+        this.authorities = authorities;
+        this.role = repRole;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(userId);
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+}
