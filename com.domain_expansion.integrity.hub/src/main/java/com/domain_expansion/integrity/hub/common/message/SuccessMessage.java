2a843bb07d7e2af4b6ac9b2e19aa7f4889765d36
bongdaehyun <pkjeogus@gmail.com>
2024-09-15 19:06:42 +0900

    fix : 성공메시지 상태값 변경 #20

--- com.domain_expansion.integrity.hub/src/main/java/com/domain_expansion/integrity/hub/common/message/SuccessMessage.java
+++ com.domain_expansion.integrity.hub/src/main/java/com/domain_expansion/integrity/hub/common/message/SuccessMessage.java
@@ -9,15 +9,15 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     SUCCESS_CREATE_HUB(HttpStatus.CREATED, "허브 등록이 완료 되었습니다."),
-    SUCCESS_GET_HUB(HttpStatus.CREATED, "허브 단건 조회가 완료 되었습니다."),
-    SUCCESS_GET_ALL_HUBS(HttpStatus.CREATED, "허브 목록 조회가 완료 되었습니다."),
-    SUCCESS_UPDATE_HUB(HttpStatus.CREATED, "허브 수정이 완료 되었습니다."),
-    SUCCESS_DELETE_HUBS(HttpStatus.CREATED, "허브 삭제가 완료 되었습니다."),
-    SUCCESS_CREATE_HUB_ROUTE(HttpStatus.CREATED, "허브 이동정보 등록이 완료 되었습니다."),
-    SUCCESS_GET_HUB_ROUTE(HttpStatus.CREATED, "허브 이동정보 단건 조회가 완료 되었습니다."),
-    SUCCESS_GET_ALL_HUBS_ROUTE(HttpStatus.CREATED, "허브 이동정보 목록 조회가 완료 되었습니다."),
-    SUCCESS_UPDATE_HUB_ROUTE(HttpStatus.CREATED, "허브 이동정보 수정이 완료 되었습니다."),
-    SUCCESS_DELETE_HUBS_ROUTE(HttpStatus.CREATED, "허브 이동정보 삭제가 완료 되었습니다.");
+    SUCCESS_GET_HUB(HttpStatus.OK, "허브 단건 조회가 완료 되었습니다."),
+    SUCCESS_GET_ALL_HUBS(HttpStatus.OK, "허브 목록 조회가 완료 되었습니다."),
+    SUCCESS_UPDATE_HUB(HttpStatus.OK, "허브 수정이 완료 되었습니다."),
+    SUCCESS_DELETE_HUBS(HttpStatus.OK, "허브 삭제가 완료 되었습니다."),
+    SUCCESS_CREATE_HUB_ROUTE(HttpStatus.OK, "허브 이동정보 등록이 완료 되었습니다."),
+    SUCCESS_GET_HUB_ROUTE(HttpStatus.OK, "허브 이동정보 단건 조회가 완료 되었습니다."),
+    SUCCESS_GET_ALL_HUBS_ROUTE(HttpStatus.OK, "허브 이동정보 목록 조회가 완료 되었습니다."),
+    SUCCESS_UPDATE_HUB_ROUTE(HttpStatus.OK, "허브 이동정보 수정이 완료 되었습니다."),
+    SUCCESS_DELETE_HUBS_ROUTE(HttpStatus.OK, "허브 이동정보 삭제가 완료 되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
