32b1976ae17355bde09beeee2b57e3e2a7878836
wonowonow <wonow_@naver.com>
2024-09-17 05:38:16 +0900

    fix: errorDecoder, Interceptor 들어가는 부분 전부 수정

---
+++ com.domain_expansion.integrity.product/src/main/java/com/domain_expansion/integrity/product/application/client/interceptor/BearerTokenRequestInterceptor.java
@@ -0,0 +1,22 @@
+package com.domain_expansion.integrity.product.application.client.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class BearerTokenRequestInterceptor implements RequestInterceptor {
+
+    private final HttpServletRequest request;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        String authorizationHeader = request.getHeader(
+                "Authorization");
+        if (authorizationHeader != null) {
+            template.header("Authorization",
+                    authorizationHeader);
+        }
+    }
+}
